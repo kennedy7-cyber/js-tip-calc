9797f55add6b332517413789567642524d0d6731
kennedy <adichronix@kali.andy>
2025-05-07 08:43:46 +0300

    tip calcuator code

---
+++ index.js
@@ -0,0 +1,7 @@
+function calculate() {
+  let bill = document.getElementById("bill amount").value;
+  let tip = document.getElementById("tip percentage").value;
+  const tipTotal = (tip / 100) * bill;
+  const totalAmount = Number(bill) + Number(tipTotal);
+  return document.getElementById("total").innerText = totalAmount;
+}
